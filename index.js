46aee22520c9f895a3f59a2fa09ca3e743327498
Muhammad Yahya <zulfikeryahya@gmail.com>
2023-09-14 14:10:22 +0600

    jwt token validate

--- index.js
+++ index.js
@@ -20,19 +20,22 @@ const port = process.env.PORT || 5000;
      deprecationErrors: true,
    }
  });
- 
-  const verifyJWT = (req, res, next)=>{
-    console.log("hit on backend")
-    console.log(req.headers.authorization)
-    const authorization = req.headers.authorization
-    if(!authorization){
-      return res.send({error: true, message: "Un authorised access"})
-    }
-    const token = authorization.split(' ')[1]
-    console.log("verify jwt token", token)
+
+ const verifyJWT = (req, res, next)=>{
+  const authorization = req.headers.authorization
+  if(!authorization){
+    res.status(401).send({error: true, message: "un authorized accesss"})
   }
-
-
+  const token = authorization.split(' ')[1]
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded)=>{
+    if(error){
+      res.status(403).send({error: true, message: "un authorized token"})
+    }
+    req.decoded = decoded
+    next()
+  })
+ }
+ 
  async function run() {
    try {
      // Connect the client to the server	(optional starting in v4.7)
@@ -78,8 +81,12 @@ const port = process.env.PORT || 5000;
       console.log(booking)
     })
 
-    app.get('/bookings', verifyJWT, async(req, res)=>{
-     // console.log(req.headers.authorization)
+    app.get('/bookings',verifyJWT, async(req, res)=>{
+      const decoded = req.decoded
+      console.log("cam back after verify", decoded)
+      if(decoded.email !== req.query.email){
+        return res.status(403).send({error: true, message: 'access is forbidden'})
+      }
       let query = {};
       if(req.query?.email) {
         query = {email: req.query.email}
